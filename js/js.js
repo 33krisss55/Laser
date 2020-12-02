768c58ca843eeed453009b8ef18aecc9afb1e945
Kristina <kkerimbaeva@keaz.ru>
2020-12-02 19:10:19 +0300

    правки

--- js/js.js
+++ js/js.js
@@ -1,4 +1,4 @@
-$('.js-examples-slider').owlCarousel({
+$('.js-about-slider').owlCarousel({
     loop:true,
     margin:20,
     nav:true,
@@ -16,6 +16,24 @@ $('.js-examples-slider').owlCarousel({
 
         769:{
             items:3,
+            nav:true
+        }
+    }
+});
+
+$('.js-video-slider').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    navText:false,
+    dots:false,
+    responsive:{
+        0:{
+            items: 1,
+            nav:true
+        },
+        769:{
+            items:2,
             nav:true
         }
     }
